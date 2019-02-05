f664394ece204e502c1f07ac3db1aef9a9c8ada5
Ivan Baklykov <ivan.baklykov@gmail.com>
2019-02-05 18:37:58 +0200

    added function for creating title table, minor changes

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-# import warnings
 import psycopg2
 import json
 
-
+# import warnings
 # import re
 # from sqlalchemy import create_engine
 
 # warnings.simplefilter('ignore')
-
-
-def from_json(filename):
-    with open(filename) as f:
-        json_data = json.load(f)
-
-    for entity in json_data:
-        print(entity)
 
 
 def conn_psql(command):
@@ -35,6 +26,18 @@ def conn_psql(command):
         conn.close()
 
 
+def from_json(filename):
+    with open(filename) as f:
+        json_data = json.load(f)
+
+    for entity in json_data:
+        print(entity)
+
+
+file = "data/cachedb_dump.json"
+from_json(file)
+
+
 def from_file(filename):
     data = []
     with open(filename, "r") as f:
@@ -49,19 +52,17 @@ def from_file(filename):
     return data
 
 
-# file = "data/cachedb_dump.json"
-# from_json(file)
+def create_title_table():
 
+    filename = "data/titles.txt"
+    data_set = from_file(filename)
 
-file = "data/titles.txt"
-data_set = from_file(file)
-
-sql = "DROP TABLE IF EXISTS public.titles"
-conn_psql(sql)
-
-sql = "CREATE TABLE public.titles ( id character varying(20) PRIMARY KEY, name character varying(200) NOT NULL )"
-conn_psql(sql)
-
-for item in data_set:
-    sql = "INSERT INTO public.titles(id, name) VALUES ('{}', '{}');".format(item['key'], item['name'])
+    sql = "DROP TABLE IF EXISTS public.titles"
     conn_psql(sql)
+
+    sql = "CREATE TABLE public.titles ( id character varying(20) PRIMARY KEY, name character varying(200) NOT NULL )"
+    conn_psql(sql)
+
+    for item in data_set:
+        sql = "INSERT INTO public.titles(id, name) VALUES ('{}', '{}');".format(item['key'], item['name'])
+        conn_psql(sql)
